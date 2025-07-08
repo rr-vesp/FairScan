d8b0b0d0dfd777dc548507590e2c65410f49241e
Pierre-Yves Nicolas <6371790+pynicolas@users.noreply.github.com>
2025-07-08 18:37:01 +0200

    AboutLibraries: generate metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.aboutLibraries)
 }
 
 android {
@@ -100,4 +101,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+aboutLibraries {
+    android.registerAndroidTasks = true
 }
