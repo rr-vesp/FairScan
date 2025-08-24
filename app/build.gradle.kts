5088110192855435c0fae793e1f9f909c77da8bc
Pierre-Yves Nicolas <6371790+pynicolas@users.noreply.github.com>
2025-08-24 10:23:20 +0200

    Prepare release: 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.mydomain.myscan"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
