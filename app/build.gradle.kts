df3b1003f9925d0aa6aeb4e4d4e285e10fe656e5
Pierre-Yves Nicolas <6371790+pynicolas@users.noreply.github.com>
2025-08-26 15:33:51 +0200

    Prepare release: v0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.fairscan.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.5.0"
+        versionCode = 8
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
