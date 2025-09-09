62fe9f3d6635307a95c784c29867d81f68b2851c
Pierre-Yves Nicolas <6371790+pynicolas@users.noreply.github.com>
2025-09-09 15:12:09 +0200

    Prepare release: v0.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         // https://ai.google.dev/edge/litert/android/index
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.12.0"
+        versionCode = 16
+        versionName = "0.13.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
